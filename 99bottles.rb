08b3cba61248a7c941a21b22558ecb0cebbcbc43
teddim <teddi@picturetheday.com>
2015-01-01 12:31:22 -0700

    pajama update

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,3 +1,6 @@
+# I overwrote my original 99 bottles (but it did work).
+# This program was to get my 6 year old daughter to get her pajamas on.
+# It worked, hahahaha.
 puts  'are you going to do what I asked you to do?'
 answer = gets.chomp
 answer = answer.downcase
@@ -5,7 +8,7 @@ while answer != 'yes'
 	songline = 'please do it, ok?'
 puts songline
 answer = gets.chomp
-	
+
 	if answer == 'yes' || answer == 'ok'
 	songline = 'Thank you! Have a cookie :).'
 puts songline
