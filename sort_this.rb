44589acf4ef49e55a1559efa133df320f492677d
teddim <teddi@picturetheday.com>
2015-01-10 17:25:59 -0700

    changed code to use an iterative swap method to solve the problem

--- sort_this.rb
+++ sort_this.rb
@@ -1,75 +1,18 @@
-def sort some_array
-  recursive_sort some_array, []
-
-end
-
-def recursive_sort unsorted_array, sorted_array
-
-  if unsorted_array == []
-    return []
-
-  else if unsorted_array.length == 1
-    return unsorted_array
-
-  else if unsorted_array.length == 2 #compare the two numbers
-
-      if unsorted_array[0] < unsorted_array[1]
-        sorted_array = [unsorted_array[0],unsorted_array[1]]
-        return sorted_array
-      else
-        sorted_array = [unsorted_array[1],unsorted_array[0]]
-        return sorted_array
-      end
-  else if unsorted_array.length >= 3 #compare the numbers
-
-    if unsorted_array[0] < unsorted_array[1]
-      if unsorted_array[0] < unsorted_array[2]
-        num = unsorted_array.length - 1
-        sorted_array = [unsorted_array[0]] + (recursive_sort (unsorted_array - [unsorted_array[0]]), sorted_array)
-        puts 'sorted_array:' + sorted_array.to_s
-        puts 'unsorted_array:' + unsorted_array.to_s
-
-        return sorted_array
-      else # unsorted_array[0] > unsorted_array[2]
-        num = unsorted_array.length - 1
-        puts 'look here'
-        puts 'sorted_array:' + sorted_array.to_s
-        puts 'unsorted_array:' + unsorted_array.to_s
-      # something is wrong here
-        sorted_array = [unsorted_array[2]] + (recursive_sort (unsorted_array- [unsorted_array[2]]), sorted_array)
-        puts 'sorted_array:' + sorted_array.to_s
-        puts 'unsorted_array:' + unsorted_array.to_s
-        return sorted_array
-      end
-    else
-      num = unsorted_array.length - 1
-      stillunsorted_array = unsorted_array[1..num] + [unsorted_array[0]]
-      sorted_array =  (recursive_sort stillunsorted_array, sorted_array)
-      #puts 'sorted_array:' + sorted_array.to_s
-      #puts 'unsorted_array:' + unsorted_array.to_s
-
-      return sorted_array
+def sort the_array
+  for i in 0..the_array.length-1
+    #puts the_array[i]
+    for j in i+1..the_array.length-1
+      if the_array[j] < the_array[i]
+        temp = the_array[j]
+        the_array[j] = the_array[i]
+        the_array[i] = temp
+      #puts '...' + the_array[j].to_s
     end
-
-    if unsorted_array[0] > unsorted_array[1]
-      #puts 'sorted_array:' + sorted_array.to_s
-      #puts 'unsorted_array:' + unsorted_array.to_s
-
-      if unsorted_array[1] < unsorted_array[2]
-        return [unsorted_array[1]]
-      else
-        return [unsorted_array[2]]
-      end
     end
 
   end
-
-  end
-  end
-  end
-
+  return the_array
 end
-
 
 puts 'array is empty. Expect []:'
 u_array = []
